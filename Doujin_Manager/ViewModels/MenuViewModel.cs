5ca85e46752e67dab60818ac41c9128d5669b960
Frank <frankstar10@gmail.com>
2018-10-17 16:58:11 -0400

    Added additional menuitems * Options: General Settings * Help: Github Repository, About * Created MenuViewModel to handle all commands related to these items

---
+++ Doujin_Manager/ViewModels/MenuViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Doujin_Manager.ViewModels
+{
+    class MenuViewModel : INotifyPropertyChanged 
+    {
+        private ICommand _openDirectoryCommand;
+        public ICommand OpenDirectoryCommand
+        {
+            get { _openDirectoryCommand = new RelayCommand(param => Process.Start("https://github.com/frankstar10/Doujin_Manager")); return _openDirectoryCommand; }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
